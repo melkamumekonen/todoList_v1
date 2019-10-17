56cf77ba284af75b680d8c17bb6ca2a31f140142
melkamumekonen <54801842+melkamumekonen@users.noreply.github.com>
2019-10-17 13:07:13 +0300

    Update app.js

--- app.js
+++ app.js
@@ -20,107 +20,170 @@ const itemSchema = new mongoose.Schema({
     required: [true, "why no task!"]
   }
 });
-
-const Item = mongoose.model('Item', itemSchema);
-const WorkItem = mongoose.model('WorkItem', itemSchema);
-
-
-Item.estimatedDocumentCount((err, num) => {
-  console.log("number of docs is : " + num);
-  if (num === 0) {
-    Item.insertMany([{
-      task: "Buy Food"
-    }, {
-      task: "Cook Food"
-    }, {
-      task: "Eat Food"
-    }], (err, docs) => console.log("inserted !!!"));
-  } else {
-    console.log("already default !!!");
-  }
+const listSchema = new mongoose.Schema({
+  name: String,
+  items: [itemSchema]
 });
+const Item = mongoose.model('Item', itemSchema);
+const List = mongoose.model("List", listSchema);
 
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
 
-// const items = ["Buy Food", "Cook Food", "Eat Food"];
-// const workItems = [];
 
 app.get("/", function (req, res) {
-
   const day = date.getDate();
 
   Item.find(function (err, docs) {
     res.render("list", {
-      listTitle: day,
+      listTitle: "Today",
       newListItems: docs
     });
-
   });
-
-  //res.render("list", {listTitle: day, newListItems: items});
-
 });
 
 app.post("/", function (req, res) {
 
   const item = req.body.newItem;
   console.log("list : " + req.body.list);
-  if (req.body.list === "Work") {
 
-    WorkItem.insertMany([{
-      task: item
-    }], (err, docs) => res.redirect("/work"));
-  } else {
-    // items.push(item);
+  List.findOne({
+    name: req.body.list
+  }, (err, doc) => {
+    if (err) {
+      console.log("error find!!!!");
+    } else {
+      if (!doc) {
+        Item.estimatedDocumentCount((err, num) => {
+          console.log("number of docs is : " + num);
+          if (num === 0) {
+            Item.insertMany([{
+              task: "Buy Food"
+            }, {
+              task: "Cook Food"
+            }, {
+              task: "Eat Food"
+            }], (err, docs) => {
+              console.log("inserted !!!");
+              res.redirect("/");
+            });
+          } else {
+            console.log("already default !!!");
+            Item.insertMany([{
+              task: item
+            }], (err, docs) => {
+              console.log("inserted !!!");
+              res.redirect("/");
+            });
+          }
+        });
+      } else {
+        console.log("found item for updating!!!!");
+        const newIt = new Item({
+          task: item
+        });
+        doc.updateOne({
+          $push: {
+            items: newIt
+          }
+        }, (err, docs) => {
+          console.log("updated successfuly!!!")
+          res.redirect("/" + doc.name);
 
-    Item.insertMany([{
-      task: item
-    }], (err, docs) => res.redirect("/"));
-    //res.redirect("/");
-  }
+        });
+      }
+    }
+  });
 });
+app.get('/favicon.ico', (req, res) => res.status(204));
 
 app.get("/:route", (req, res) => {
-res.send(req.params);
-});
-
-
-
-// app.get("/work", function (req, res) {
-
-//   WorkItem.find(function (err, docs) {
-//     res.render("list", {
-//       listTitle: "Work List",
-//       newListItems: docs.map(e => e.task)
-//     });
-
-//   });
-// });
-
-// app.get("/about", function (req, res) {
-//   res.render("about");
-// });
-
-app.post("/delete", function (req, res) {
-  console.log(req.body);
-  Item.deleteOne({
-    _id : req.body.item
-  }, (err, docs) => {
+  const costumListName = req.params.route;
+  console.log("route is : " + req.params.route);
+  List.findOne({
+    name: costumListName
+  }, (err, doc) => {
+    console.log("doc is : " + doc);
     if (err) {
-      console.log(err);
+      console.log("error finding document!!");
     } else {
-      console.log("succefuly deleted !");
+      if (!doc) {
+        console.log("not found!!!");
+        const list = new List({
+          name: costumListName,
+          items: []
+        });
+        list.save().then(() => {
+          res.redirect("/" + costumListName);
+        });
+      } else {
+        console.log("found!!!!" + doc);
+        res.render("list", {
+          listTitle: doc.name,
+          newListItems: doc.items
+        });
+
+
+      }
     }
-    res.redirect("/");
   });
 });
 
+app.post("/delete", function (req, res) {
+  console.log("delete params : " + JSON.stringify(req.body));
+  const list = req.body.listName;
+  if (list === "Today") {
+    Item.deleteOne({
+      _id: req.body.item
+    }, (err, docs) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("succefuly deleted !");
+      }
 
+      Item.estimatedDocumentCount((err, num) => {
+        console.log("number of docs is : " + num);
+        if (num === 0) {
+          Item.insertMany([{
+            task: "Buy Food"
+          }, {
+            task: "Cook Food"
+          }, {
+            task: "Eat Food"
+          }], (err, docs) => {
+            console.log("inserted !!!");
+            res.redirect("/");
+          });
+        } else {
+          console.log("already default !!!");
+          res.redirect("/");
+        }
+      });
+    });
+  } else {
+    List.findOne({
+      name: list
+    }, (err, doc) => {
+      if (err) {
+        console.log("Error deleting item from custom list !");
+      } else {
+
+        console.log("Before deleting items custom list : " + doc.items + "before!");
+        doc.items = doc.items.filter((e) => {
+          return e._id != req.body.item
+        });
+        console.log("After deleting items custom list : " + doc.items + "after!");
+        doc.save().then(() => {
+          res.redirect("/" + list);
+        });
+      }
+    });
+  }
+});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
